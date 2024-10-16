5b881c379b3efaae66dce04fe8a395d66b53115e
Mahdi-Zafari <zafarim880@gmail.com>
2024-10-16 19:19:28 +0330

    Update power-options.script.js

--- server/static/js/power-options.script.js
+++ server/static/js/power-options.script.js
@@ -3,26 +3,21 @@ document.querySelectorAll('.power-option').forEach(button => {
         const action = button.id;
         showLoading(action);
 
-        fetch(`/${action}`)
-            .then(response => {
-                if (response.ok) {
-                    return response.text();
-                } else {
-                    throw new Error('Error: ' + response.status);
-                }
-            })
-            .then(() => {
-                showMessage('success', 'Operation was successful!');
-
+        $.ajax({
+            url: `/${action}`,
+            type: 'POST',
+            success: function(response) {
+                showMessage('success', `${action.charAt(0).toUpperCase() + action.slice(1)} was successful!`);
                 setTimeout(() => {
                     window.location.href = '/operation_status';
                 }, 1000);
-            })
-            .catch(error => {
-                console.error(error);
-                showMessage('error', 'An error occurred: ' + error.message);
+            },
+            error: function(xhr, status, error) {
+                console.error(`Error: ${xhr.status} - ${error}`);
+                showMessage('error', `An error occurred: ${xhr.responseText || error}`);
                 hideLoading();
-            });
+            }
+        });
     });
 });
 
@@ -43,29 +38,17 @@ function showMessage(type, message) {
     const errorMessage = document.getElementById('error-message');
 
     if (type === 'success') {
-        if (successMessage) {
-            successMessage.textContent = message;
-            successMessage.style.display = 'block';
-        }
-        if (errorMessage) {
-            errorMessage.style.display = 'none';
-        }
+        successMessage.textContent = message;
+        successMessage.style.display = 'block';
+        errorMessage.style.display = 'none';
     } else if (type === 'error') {
-        if (errorMessage) {
-            errorMessage.textContent = message;
-            errorMessage.style.display = 'block';
-        }
-        if (successMessage) {
-            successMessage.style.display = 'none';
-        }
+        errorMessage.textContent = message;
+        errorMessage.style.display = 'block';
+        successMessage.style.display = 'none';
     }
 
     setTimeout(() => {
-        if (successMessage) {
-            successMessage.style.display = 'none';
-        }
-        if (errorMessage) {
-            errorMessage.style.display = 'none';
-        }
+        successMessage.style.display = 'none';
+        errorMessage.style.display = 'none';
     }, 5000);
 }
